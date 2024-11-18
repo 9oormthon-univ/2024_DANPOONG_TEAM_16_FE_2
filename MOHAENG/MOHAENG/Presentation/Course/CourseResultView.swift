fa53661e0162307fe652c78ea6f600833ecbd281
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-18 19:56:58 +0900

    feat : 코스 결과 화면 임시 문구 구성

--- MOHAENG/MOHAENG/Presentation/Course/CourseResultView.swift
+++ MOHAENG/MOHAENG/Presentation/Course/CourseResultView.swift
@@ -11,8 +11,28 @@ struct CourseResultView: View {
     
     @State private var draw: Bool = true
     
+    private let place = "속초"
+    private let period = "2박3일"
+    private let mention = "더욱 쉽게 접근할 수 있는 코스로 준비했어요"
+    
+    private let semiboldFontSize: CGFloat = 20
+    private let mediumFontSize: CGFloat = 18
+    
     var body: some View {
         NavigationStack {
+            VStack {
+                Image(uiImage: .mountain)
+                HStack {
+                    Text(place)
+                        .font(.system(size: semiboldFontSize, weight: .semibold))
+                    Text(period)
+                        .font(.system(size: semiboldFontSize, weight: .semibold))
+                }
+                .padding(3)
+                Text(mention)
+                    .font(.system(size: mediumFontSize, weight: .medium))
+                    .padding(3)
+                
                 GeometryReader { geometry in
                     MapView(draw: $draw)
                         .onAppear {
