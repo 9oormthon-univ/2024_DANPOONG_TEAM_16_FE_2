aacbd5dc12469fd754210f2be196191ecd8c3b6a
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-18 16:16:03 +0900

    init : 프로젝트 생성

---
+++ MOHAENG/MOHAENG/MOHAENGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOHAENGApp.swift
+//  MOHAENG
+//
+//  Created by 박동재 on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MOHAENGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
