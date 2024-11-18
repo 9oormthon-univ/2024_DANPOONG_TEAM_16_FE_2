190ba8eb66c484d46167afc80f6987c5612d944e
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-18 19:31:36 +0900

    feat : 시작 시, Key값 전달

--- MOHAENG/MOHAENG/MOHAENGApp.swift
+++ MOHAENG/MOHAENG/MOHAENGApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import KakaoMapsSDK
 
 @main
 struct MOHAENGApp: App {
+    
+    init() {
+        setup()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    private func setup() {
+        guard let KAKAO_API_KEY = Bundle.main.KAKAO_API_KEY else { return }
+        SDKInitializer.InitSDK(appKey: KAKAO_API_KEY)
     }
 }
