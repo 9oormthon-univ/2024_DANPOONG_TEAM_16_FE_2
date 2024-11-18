5868ee4c0d2738cf162c8257d06e81bec27baa25
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-18 17:40:48 +0900

    init : Key 관련 세팅

---
+++ MOHAENG/MOHAENG/Extension/Bundle+.swift
@@ -0,0 +1,20 @@
+//
+//  Bundle+.swift
+//  MOHAENG
+//
+//  Created by 박동재 on 11/18/24.
+//
+
+import Foundation
+
+extension Bundle {
+    
+    var KAKAO_API_KEY: String? {
+        return infoDictionary?["KAKAO_API_KEY"] as? String
+    }
+    
+    var WEATHER_KEY: String? {
+        return infoDictionary?["WEATHER_KEY"] as? String
+    }
+    
+}
