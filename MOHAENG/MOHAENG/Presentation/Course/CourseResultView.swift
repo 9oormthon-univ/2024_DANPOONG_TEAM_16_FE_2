afbe1b238b76a465e5d9594d9c48247aa0339e7d
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-19 13:03:29 +0900

    feat : 코스 결과 화면에 DayComponent 구성

--- MOHAENG/MOHAENG/Presentation/Course/CourseResultView.swift
+++ MOHAENG/MOHAENG/Presentation/Course/CourseResultView.swift
@@ -10,30 +10,38 @@ import SwiftUI
 struct CourseResultView: View {
     
     @State private var draw: Bool = true
+    @State var selectedDay: Int = 0
     
     private let place = "속초"
     private let period = "2박3일"
     private let mention = "더욱 쉽게 접근할 수 있는 코스로 준비했어요"
+    
+    private let daysList: [String] = ["1일차", "2일차", "3일차"]
+    
+    private let backgroundColor = "0A70C9"
+    private let defalutColor = "E2E2E2"
+    private let fontColor = "FFFFFF"
     
     private let semiboldFontSize: CGFloat = 20
     private let mediumFontSize: CGFloat = 18
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Image(uiImage: .mountain)
-                HStack {
-                    Text(place)
-                        .font(.system(size: semiboldFontSize, weight: .semibold))
-                    Text(period)
-                        .font(.system(size: semiboldFontSize, weight: .semibold))
-                }
-                .padding(3)
-                Text(mention)
-                    .font(.system(size: mediumFontSize, weight: .medium))
+            
+            GeometryReader { geometry in
+                VStack {
+                    Image(uiImage: .mountain)
+                    HStack {
+                        Text(place)
+                            .font(.system(size: semiboldFontSize, weight: .semibold))
+                        Text(period)
+                            .font(.system(size: semiboldFontSize, weight: .semibold))
+                    }
                     .padding(3)
-                
-                GeometryReader { geometry in
+                    Text(mention)
+                        .font(.system(size: mediumFontSize, weight: .medium))
+                        .padding(3)
+                    
                     MapView(draw: $draw)
                         .onAppear {
                             self.draw = true
@@ -41,12 +49,21 @@ struct CourseResultView: View {
                         .onDisappear{
                             self.draw = false
                         }
-                        .frame(height: geometry.size.height * 0.3)
+                        .frame(height: geometry.size.height * 0.35)
+                    
+                    DaysComponent(
+                        selectedDay: $selectedDay,
+                        daysList: daysList,
+                        backgroundColor: backgroundColor,
+                        defalutColor: defalutColor,
+                        fontColor: fontColor
+                    )
+                    
+                    
                 }
             }
         }
     }
-    
 }
 
 #Preview {
