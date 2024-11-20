bd4aa425382de0272654afe22157eb620fb0d478
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-20 13:33:40 +0900

    feat : 바텀시트 화면 구성

---
+++ MOHAENG/MOHAENG/Presentation/Detail/DetailBottomSheetView.swift
@@ -0,0 +1,37 @@
+//
+//  DetailBottomSheetView.swift
+//  MOHAENG
+//
+//  Created by 박동재 on 11/19/24.
+//
+
+import SwiftUI
+
+struct DetailBottomSheetView: View {
+    
+    @Binding var offset: CGFloat
+    let height: CGFloat
+    
+    var body: some View {
+        ZStack {
+            DetailSegmentView()
+                .clipShape(CustomCorner(corners: [.topLeft, .topRight], radius: 30))
+            
+            VStack {
+                VStack {
+                    Capsule()
+                        .fill(Color.black)
+                        .frame(width: 120, height: 4)
+                }
+                .frame(height: 40)
+            }
+            .padding(.horizontal)
+            .frame(maxHeight: .infinity, alignment: .top)
+        }
+    }
+    
+}
+
+#Preview {
+    DetailResultView()
+}
