f301af3c06438dd90c1725f38a41b61070021979
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-20 17:18:05 +0900

    feat : 코스 열람 화면 날씨 화면 생성

---
+++ MOHAENG/MOHAENG/Presentation/Detail/DetailWeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailWeatherView.swift
+//  MOHAENG
+//
+//  Created by 박동재 on 11/19/24.
+//
+
+import SwiftUI
+
+struct DetailWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailWeatherView()
+}
