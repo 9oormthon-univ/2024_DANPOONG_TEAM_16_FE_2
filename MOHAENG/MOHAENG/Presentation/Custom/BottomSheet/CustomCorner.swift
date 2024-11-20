4e954d67cb2355b2591ecafcdd51f2ffe9ce0d8b
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-20 13:30:05 +0900

    feat : 커스텀 바텀시트를 위한 코너 구성

---
+++ MOHAENG/MOHAENG/Presentation/Custom/BottomSheet/CustomCorner.swift
@@ -0,0 +1,21 @@
+//
+//  CustomCorner.swift
+//  MOHAENG
+//
+//  Created by 박동재 on 11/19/24.
+//
+
+import SwiftUI
+
+struct CustomCorner: Shape {
+    
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+    
+}
