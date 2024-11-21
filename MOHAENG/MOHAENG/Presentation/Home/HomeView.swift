9a4fb71cf230b7c20bc4cad9d74824bfe0766cf7
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-21 15:30:24 +0900

    style : 홈 화면 이미지 추가

---
+++ MOHAENG/MOHAENG/Presentation/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  MOHAENG
+//
+//  Created by 박동재 on 11/21/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
