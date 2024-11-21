7ca0aa6d4a37c32f10fd3886c95e67723544ed7f
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-21 15:03:10 +0900

    feat : Weather 모델 생성

---
+++ MOHAENG/MOHAENG/Weather.swift
@@ -0,0 +1,16 @@
+//
+//  Weather.swift
+//  MOHAENG
+//
+//  Created by 박동재 on 11/21/24.
+//
+
+import Foundation
+
+struct Weather: Hashable {
+    let day: String
+    let date: String
+    let weather: String
+    let highTemperture: String
+    let lowTemperture: String
+}
