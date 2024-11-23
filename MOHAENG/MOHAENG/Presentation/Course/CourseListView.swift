c1be056dbdc682ee0f7359129fec3787cf73ccf2
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-23 21:52:31 +0900

    feat : 데이터 연결

--- MOHAENG/MOHAENG/Presentation/Course/CourseListView.swift
+++ MOHAENG/MOHAENG/Presentation/Course/CourseListView.swift
@@ -9,55 +9,89 @@ import SwiftUI
 
 struct CourseListView: View {
     
-    @State private var numbers: [Int] = [ 1, 2, 3, 4, 5 ]
+    @Binding var course: CourseDTO
+    @Binding var selectedDay: Int
+    
+    @Binding var dayOneList: [Course]
+    @Binding var dayTwoList: [Course]
+    @Binding var dayThreeList: [Course]
+    
+    @State private var selectedList: [Course] = []
+    
+    @State private var numbers: [Course] = [
+        Course(name: "1", address: "2", imageUrl: "3"),
+        Course(name: "4", address: "5", imageUrl: "6")
+    ]
     @State private var isWebViewShowing: Bool = false
     
+    init(
+        course: Binding<CourseDTO>,
+        selectedDay: Binding<Int>,
+        dayOneList: Binding<[Course]>,
+        dayTwoList: Binding<[Course]>,
+        dayThreeList: Binding<[Course]>
+    ) {
+        self._course = course
+        self._selectedDay = selectedDay
+        self._dayOneList = dayOneList
+        self._dayTwoList = dayTwoList
+        self._dayThreeList = dayThreeList
+    }
+    
     var body: some View {
-        List {
-            ForEach(numbers, id: \.self) { index in
-                HStack {
-                    VStack {
-                        ZStack(alignment: .center) {
-                            Image(uiImage: .placeMarker)
-                                .resizable()
-                                .scaledToFit()
-                            Text("\(index)")
-                                .font(.system(size: 12))
-                                .foregroundColor(.white)
-                                .padding(.bottom, 3)
-                        }
-                            .frame(height: 28)
-                        
-                        Image(uiImage: .dotLine)
+        
+        ForEach($selectedList, id: \.self) { day in
+            HStack {
+                VStack {
+                    ZStack(alignment: .center) {
+                        Image(uiImage: .placeMarker)
                             .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 40)
+                            .scaledToFit()
+                        Text(" ")
+                            .font(.system(size: 12))
+                            .foregroundColor(.white)
+                            .padding(.bottom, 3)
                     }
-                    CourseView()
-                        .cornerRadius(15)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 15)
-                                .stroke(lineWidth: 0.2)
-                                .foregroundColor(.gray)
-                        )
+                    .frame(height: 28)
+                    
+                    Image(uiImage: .dotLine)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 40)
                 }
-                .listRowSeparator(.hidden)
+                CourseView(day: day)
+                    .cornerRadius(15)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(lineWidth: 0.2)
+                            .foregroundColor(.gray)
+                    )
             }
-            .onDelete(perform: { indexSet in
-                numbers.remove(atOffsets: indexSet)
-            })
-            .onMove(perform: { indices, newOffset in
-                numbers.move(fromOffsets: indices, toOffset: newOffset)
-            })
-            .onTapGesture {
-                self.isWebViewShowing.toggle()
+            .listRowSeparator(.hidden)
+        }
+        .onDelete(perform: { indexSet in
+            numbers.remove(atOffsets: indexSet)
+        })
+        .onMove(perform: { indices, newOffset in
+            numbers.move(fromOffsets: indices, toOffset: newOffset)
+        })
+        .onTapGesture {
+            self.isWebViewShowing.toggle()
+        }
+        .onChange(of: selectedDay) { _, newValue in
+            dump(course)
+            if newValue == 1 {
+                selectedList = dayOneList
+            } else if newValue == 2 {
+                selectedList = dayTwoList
+            } else if newValue == 3 {
+                selectedList = dayThreeList
             }
         }
-        .listStyle(PlainListStyle())
-        .scrollIndicators(.hidden)
         .fullScreenCover(isPresented: $isWebViewShowing, content: {
             CourseWebView()
         })
     }
+
 }
 
--- MOHAENG/MOHAENG/Presentation/Course/CourseView.swift
+++ MOHAENG/MOHAENG/Presentation/Course/CourseView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct CourseView: View {
+    
+    @Binding var day: Course
+    
+    init(day: Binding<Course>) {
+        self._day = day
+    }
+    
     var body: some View {
         HStack {
-            Text("IMG")
-                .padding()
+            if let url = URL(string: day.imageUrl) {
+                AsyncImage(url: url) { image in
+                    image.image?.resizable()
+                }
+                .frame(maxWidth: 55, maxHeight: 64)
+                .cornerRadius(4)
+                .background(Color(.white))
+                .clipped()
+            } else {
+                Image(uiImage: .placeholder)
+                    .resizable()
+                    .cornerRadius(4)
+                    .frame(maxWidth: 55, maxHeight: 64)
+                    .background(Color(hex: "#F6F5FA"))
+                    .clipped()
+            }
             
             VStack(alignment: .leading, content: {
-                Text("Title")
-                Text("Address")
+                Text("\(day.name)")
+                    .font(.system(size: 16, weight: .semibold))
+                Text("\(day.address)")
+                    .font(.system(size: 12, weight: .medium))
             })
             
             Spacer()
@@ -25,6 +48,7 @@ struct CourseView: View {
         }
         .padding()
     }
+    
 }
 
 #Preview {
